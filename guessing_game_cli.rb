966d36b0baf66c90f3331c23d3ca18a9b1cb48b8
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-29 19:25:37 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -12,3 +12,15 @@ end
 def user_guess
   gets.chomp
 end
+
+def evaluate_game(comp_num, user_num)
+  if user_num == 'exit'
+    puts "Goodbye!"
+  elsif comp_num == user_num.to_i
+    puts "You guessed the correct number!"
+  elsif comp_num != user_num.to_i
+    puts "Sorry! The computer guessed #{comp_num}."
+  else
+    puts "Something else is wrong"
+  end
+end
